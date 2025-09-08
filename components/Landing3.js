3349e14bc4443195b02cb8cafdd065222f79f4e4
Capacitor <rohitkhengare172@gmail.com>
2025-09-08 19:53:57 +0530

    added next/link to fix build time error

--- components/Landing3.js
+++ components/Landing3.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Landing3 = () => {
   return (
@@ -8,7 +9,7 @@ const Landing3 = () => {
             <h2 className='text-4xl font-bold'><span>Lorem ipsum dolor sit amet consectetur </span> <span className='text-[#fbbc04]'>adipisicing elit. Consequuntur iure cumque</span></h2>
             </div>
             <div className='w-1/2 text-xl'>
-                <p>Lorem <a className='text-blue-600 hover:underline' href="/">ipsum, dolor</a> sit amet consectetur adipisicing elit. <a className='text-blue-600 hover:underline' href="/">Eligendi provident</a> libero qui quis, aspernatur dolore!</p>
+                <p>Lorem <Link className='text-blue-600 hover:underline' href="/">ipsum, dolor</Link> sit amet consectetur adipisicing elit. <Link className='text-blue-600 hover:underline' href="/">Eligendi provident</Link> libero qui quis, aspernatur dolore!</p>
                 <button className='p-3 my-4 rounded-full bg-white text-blue-700 '>Get started for free</button>
             </div>
         </div>

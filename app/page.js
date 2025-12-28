c0f906b37e6b2009555fd398cd2b54e58b82560f
Capacitor <rohitkhengare172@gmail.com>
2025-12-28 19:34:46 +0530

    added landing4 component

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Navigation from "@/components/Navigation";
 import Mainlanding1 from "@/components/Mainlanding1";
 import Landing2 from "@/components/Landing2";
 import Landing3 from "@/components/Landing3";
+import Landing4 from "@/components/Landing4";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     <Mainlanding1></Mainlanding1>
     <Landing2></Landing2>
     <Landing3></Landing3>
+    <Landing4></Landing4>
    </>
   );
 }
---
+++ components/Landing4.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Landing4 = () => {
+  return (
+    <div className='text-white p-5 bg-linear-180 from-blue-600 to-orange-300'>
+        <div className="part1 mx-auto py-15 flex flex-col md:flex-row justify-between max-w-6xl ">
+            <div className='text-4xl font-bold '>Lorem ipsum dolor sit amet.</div>
+            <div className='text-xl md:w-1/2 py-5 md:py-0 ' >
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit eveniet distinctio illo quaerat similique facere explicabo sequi! Quod, dolor in?</p>
+            <button className='p-3 my-4 text-sm rounded-full bg-white text-blue-700 '>Get started for free</button>
+            </div>
+        </div>
+        <div className="part2 border-2 border-white p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+            <div className="box text-lg border border-white p-5 rounded-sm backdrop-brightness-75 hover:text-black hover:bg-white">
+                <div className='text-4xl pb-3'>🎨</div>
+                <div><h4 className='font-bold py-2'>Lorem, ipsum dolor sit amet consectetur adipisicing.</h4></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eaque, nemo sunt odio distinctio accusantium ducimus incidunt magni autem, maxime, esse quaerat illum provident nostrum.</p>
+            </div>
+            <div className="box text-lg border border-white p-5 rounded-sm backdrop-brightness-75 hover:text-black hover:bg-white">
+                <div className='text-4xl pb-3'>🎨</div>
+                <div><h4 className='font-bold py-2'>Lorem, ipsum dolor sit amet consectetur adipisicing.</h4></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eaque, nemo sunt odio distinctio accusantium ducimus incidunt magni autem, maxime, esse quaerat illum provident nostrum.</p>
+            </div>
+            <div className="box text-lg border border-white p-5 rounded-sm backdrop-brightness-75 hover:text-black hover:bg-white">
+                <div className='text-4xl pb-3'>🎨</div>
+                <div><h4 className='font-bold py-2'>Lorem, ipsum dolor sit amet consectetur adipisicing.</h4></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eaque, nemo sunt odio distinctio accusantium ducimus incidunt magni autem, maxime, esse quaerat illum provident nostrum.</p>
+            </div>
+            <div className="box text-lg border border-white p-5 rounded-sm backdrop-brightness-75 hover:text-black hover:bg-white">
+                <div className='text-4xl pb-3'>🎨</div>
+                <div><h4 className='font-bold py-2'>Lorem, ipsum dolor sit amet consectetur adipisicing.</h4></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eaque, nemo sunt odio distinctio accusantium ducimus incidunt magni autem, maxime, esse quaerat illum provident nostrum.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing4
